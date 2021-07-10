b50c8bff96bb72c0bb537ee7c968d0761f0268f7
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2021-07-10 20:49:59 -0300

    Inserting id with uuid

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 app.use(express.json());
-app.use(categoriesRoutes);
+app.use('/categories', categoriesRoutes);
 
 app.listen(3333, () => {
   console.log('Server is running ✨');
