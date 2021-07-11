0adca2918f60321d942671002b980976f90dc64f
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2021-07-11 15:48:01 -0300

    Fixing importations

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,7 @@
 import { Router } from 'express';
-import { v4 as uuidv4 } from 'uuid';
 
-import { Category } from '../model/Category';
-import { CategoriesRepository } from '../repositories/CategoriesRepository';
-import { CreateCategoryService } from '../service/CreateCategoryService';
+import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
+import { CreateCategoryService } from '../modules/cars/service/CreateCategoryService';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
