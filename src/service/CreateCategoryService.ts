05e0fda8cc56c5c12079c8bd3273860ebc76f9ce
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2021-07-11 15:30:08 -0300

    Using the Liskov Substitution Principle (LSP)

--- src/service/CreateCategoryService.ts
+++ src/service/CreateCategoryService.ts
@@ -1,4 +1,5 @@
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { ICategoriesRepository } from '../repositories/ICategoriesRepository';
 
 interface IRequest {
   name: string;
@@ -6,12 +7,12 @@ interface IRequest {
 }
 
 class CreateCategoryService {
-  constructor(private categoriesRepository: CategoriesRepository) {}
+  constructor(private categoriesRepository: ICategoriesRepository) {}
 
   execute({ name, description }: IRequest): void {
-    const categoryAlredyExists = this.categoriesRepository.findByName(name);
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
 
-    if (categoryAlredyExists) {
+    if (categoryAlreadyExists) {
       throw new Error('Category already exists!');
     }
 
