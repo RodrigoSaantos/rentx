602939a38a1f774f51bea5b277e039e6c1536fde
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2021-07-27 23:32:22 -0300

    Documentation Import and Specifications

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -9,7 +9,7 @@ class ImportCategoryController {
 
     const { file } = request;
     await importCategoryUseCase.execute(file);
-    return response.send();
+    return response.status(201).send();
   }
 }
 
