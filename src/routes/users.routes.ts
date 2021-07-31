363df6eced94afdaf79c850231d9d82cffd64ea2
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2021-07-31 19:13:06 -0300

    Adding avatar column

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,19 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { CreateUserController } from '../modules/accounts/useCases/createUser/CreateUserController';
+import { UpdateUserAvatarController } from '../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
 
 const usersRoutes = Router();
 
+const upload = multer({
+  dest: './avatar',
+});
+
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRoutes.post('/', createUserController.handle);
+usersRoutes.patch('/avatar', updateUserAvatarController.handle);
 
 export { usersRoutes };
