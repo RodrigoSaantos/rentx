07e06cc7bc60c74c08abd4b49af866e5d376b72a
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2021-08-01 13:13:23 -0300

    Refactoring imports

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,9 @@ import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
 import './database';
-import './shared/container';
-import { AppError } from './errors/AppError';
+import '@shared/container';
+import { AppError } from '@errors/AppError';
+
 import { router } from './routes';
 import swaggerFile from './swagger.json';
 
