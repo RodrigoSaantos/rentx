7c1ed85a1d3f52df2279e5b15705b2ca4aeed986
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2021-08-05 23:04:15 -0300

    Creating user seed

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,12 +2,14 @@ import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
-import '@shared/infra/typeorm';
 import '@shared/container';
 import { AppError } from '@shared/errors/AppError';
 import { router } from '@shared/infra/http/routes';
+import createConnection from '@shared/infra/typeorm';
 
 import swaggerFile from '../../../swagger.json';
+
+createConnection();
 
 const app = express();
 app.use(express.json());
