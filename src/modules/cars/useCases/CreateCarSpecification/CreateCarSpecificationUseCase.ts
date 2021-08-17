287aab88cffc0e6ca57f038343f157b9b00f98a8
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2021-08-16 21:29:15 -0300

    Continuation of CreateCarSpecificationUseCase

--- src/modules/cars/useCases/CreateCarSpecification/CreateCarSpecificationUseCase.ts
+++ src/modules/cars/useCases/CreateCarSpecification/CreateCarSpecificationUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from 'tsyringe';
 
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationsRepository';
 import { AppError } from '@shared/errors/AppError';
 
 interface IRequest {
@@ -13,6 +14,8 @@ class CreateCarSpecificationUseCase {
   constructor(
     // @inject('CarsRepository')
     private carsRepository: ICarsRepository,
+
+    private specificationsRepository: ISpecificationsRepository,
   ) {}
   async execute({ car_id, specifications_id }: IRequest): Promise<void> {
     const carExists = await this.carsRepository.findById(car_id);
@@ -20,6 +23,14 @@ class CreateCarSpecificationUseCase {
     if (!carExists) {
       throw new AppError('Car does not exists!');
     }
+
+    const specifications = await this.specificationsRepository.findByIds(
+      specifications_id,
+    );
+
+    carExists.specifications = specifications;
+
+    await this.carsRepository.create(carExists);
   }
 }
 
