e48ef49ef349ad41da9d05b6f1abb577830946b2
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2022-01-04 09:25:45 -0300

    Continuation of rental registration

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { RentalsRepositoryInMemory } from '@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory';
+import { AppError } from '@shared/errors/AppError';
 
 import { CreateRentalUseCase } from './CreateRentalUseCase';
 
@@ -11,11 +12,46 @@ describe('Create Rental', () => {
     createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory);
   });
 
-  it('Shoulbe able to create a new rental', async () => {
-    await createRentalUseCase.execute({
+  it('Should be able to create a new rental', async () => {
+    const rental = await createRentalUseCase.execute({
       user_id: '12345',
       car_id: '121212',
       expected_return_date: new Date(),
     });
+
+    console.log(rental);
+
+    expect(rental).toHaveProperty('id');
+    expect(rental).toHaveProperty('start_date');
+  });
+
+  it('Should not be able to create a new rental if there is another open to the same user', async () => {
+    expect(async () => {
+      await createRentalUseCase.execute({
+        user_id: '12345',
+        car_id: '121212',
+        expected_return_date: new Date(),
+      });
+      await createRentalUseCase.execute({
+        user_id: '12345',
+        car_id: '121212',
+        expected_return_date: new Date(),
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('Should not be able to create a new rental if there is another open to the same car', async () => {
+    expect(async () => {
+      await createRentalUseCase.execute({
+        user_id: '123',
+        car_id: 'test',
+        expected_return_date: new Date(),
+      });
+      await createRentalUseCase.execute({
+        user_id: '321',
+        car_id: 'test',
+        expected_return_date: new Date(),
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
