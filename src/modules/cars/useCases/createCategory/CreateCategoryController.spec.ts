941821fe29772db63a7e50c9604b0f55fbba7d95
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2022-01-06 21:33:43 -0300

    Creating the first integration test

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -3,7 +3,12 @@ import request from 'supertest';
 import { app } from '@shared/infra/http/app';
 
 describe('Create category controller', async () => {
-  it('tes', async () => {
-    await request(app).get('/cars/available').expect(201);
+  it('Should be able to create a new category', async () => {
+    const response = await request(app).post('/categories').send({
+      name: 'Category Supertest',
+      description: 'Category Supertest description',
+    });
+
+    expect(response.status).toBe(201);
   });
 });
